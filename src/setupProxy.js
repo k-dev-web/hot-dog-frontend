ee9dd89f0b84eeeac8b74b9af39a78a4ff025642
redjonaldo@gmail.com <H1k1komor1591>
2021-03-02 10:25:41 +0200

    add proxy middleware

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,10 @@
-import { createProxyMiddleware, } from 'http-proxy-middleware';
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(
-        '/**',
+        '/api',
         createProxyMiddleware({
-            target: 'https://hot-dog-backend.herokuapp.com',
+            target: 'https://hot-dog-backend.herokuapp.com/',
             changeOrigin: true,
         })
     );
