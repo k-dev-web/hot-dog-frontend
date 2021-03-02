e65682f8fad0ad5f90154c46efc5f9e38047ed03
redjonaldo@gmail.com <H1k1komor1591>
2021-03-02 09:29:44 +0200

    add proxy middleware

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+import { createProxyMiddleware, } from 'http-proxy-middleware';
+
+module.exports = function(app) {
+    app.use(
+        '/**',
+        createProxyMiddleware({
+            target: 'https://hot-dog-backend.herokuapp.com',
+            changeOrigin: true,
+        })
+    );
+};
